411f13fc2404fb7d671da960f41e996f16cc1e11
Mruruc <mr.uruc21@gmail.com>
2024-09-05 23:36:18 +0200

    minor bug fix in start.sh script

--- nginx/start.sh
+++ nginx/start.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
+echo $VITE_APP_API_URL
 # Replace the placeholder API_URL with the environment variable value
-sed -i 's|API_URL|'"${VITE_APP_API_URL}"'|' /etc/nginx/nginx.conf
+sed -i 's|API-URL|'echo $VITE_APP_API_URL'|' /etc/nginx/nginx.conf
 
 # Start NGINX
 nginx -g "daemon off;"
